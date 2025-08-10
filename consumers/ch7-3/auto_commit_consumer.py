883fb787e7db80d526f459be59c07f46157ec6be
squirrrrrrel0709 <poppinmiji@gmail.com>
2025-08-10 16:32:19 +0900

    73

--- consumers/ch7-3/auto_commit_consumer.py
+++ consumers/ch7-3/auto_commit_consumer.py
@@ -7,36 +7,38 @@ import json
 
 class AutoCommitConsumer(BaseConsumer):
     def __init__(self, group_id):
-        super().__init__(group_id)
-        self.topics = ['apis.seouldata.rt-bicycle']
+        super().__init__(group_id) #상속:서버,로거,group_id 받아옴 -> consumer 생성
+        self.topics = ['apis.seouldata.rt-bicycle'] # 구독할 토픽이름 -> 구독할때 필요
 
-        conf = {'bootstrap.servers': self.BOOTSTRAP_SERVERS,
+        conf = {'bootstrap.servers': self.BOOTSTRAP_SERVERS, #컨슈머 설정
                 'group.id': self.group_id,
-                'auto.offset.reset': 'latest',
-                'enable.auto.commit': 'true',
-                'auto.commit.interval.ms': '60000'         # 기본 값: 5000 (5초)
+                'auto.offset.reset': 'latest', # 최근거부터 마저읽기!
+                'enable.auto.commit': 'true', # earliest에서 true로 바뀜
+                'auto.commit.interval.ms': '60000'   # 기본 값: 5000 (5초) -> 60초
                 }
 
-        self.consumer = Consumer(conf)
+        self.consumer = Consumer(conf) #라이브러리 제공 클래스로 컨슈머 생성
+        # 해당토픽 구독, 성공시 callback_on_assign 실행
         self.consumer.subscribe(self.topics, on_assign=self.callback_on_assign)
 
 
     def poll(self):
         try:
-            while True:
+            while True:                       #메세지 100개 읽어서 리스트 저장
                 msg_lst = self.consumer.consume(num_messages=100)
-                if msg_lst is None or len(msg_lst) == 0: continue
+                if msg_lst is None or len(msg_lst) == 0: continue # 메세지 없으면 다음루프
 
-                self.logger.info(f'message count:{len(msg_lst)}')
+                self.logger.info(f'message count:{len(msg_lst)}') #메세지길이 로그찍기
                 for msg in msg_lst:
                     error = msg.error()
                     if error:
-                        self.handle_error(msg, error)
+                        self.handle_error(msg, error) # 베이스컨슈머 메서드
 
                 # 로직 처리 부분
-                # Kafka 레코드에 대한 전처리, Target Sink 등 수행
                 self.logger.info(f'message 처리 로직 시작')
+                # 메세지 값을 디코딩하고, JSON문자열을 파이썬 객체로 반환
                 msg_val_lst = [json.loads(msg.value().decode('utf-8')) for msg in msg_lst]
+                # DF로 만들어서 찍어줌
                 df = pd.DataFrame(msg_val_lst)
                 print(df[:10])
 
